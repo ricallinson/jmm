b3cc70acf2d24e0abaf1872e7535bd8618cd8b4a
Ric Allinson <allinson@yahoo-inc.com>
2016-01-22 23:23:58 -0800

    Added build example

---
+++ examples/src/github/com/ricallinson/jmmbuild/BuildClass.java
@@ -0,0 +1,10 @@
+package github.com.ricallinson.jmmbuild;
+
+import github.com.ricallinson.jmmbuild.sub.*;
+
+class BuildClass {  
+    public static void main(String args[]) {
+        OtherClass c = new OtherClass();
+        System.out.println(c.msg());
+    }
+}
---
+++ examples/src/github/com/ricallinson/jmmbuild/sub/OtherClass.java
@@ -0,0 +1,7 @@
+package github.com.ricallinson.jmmbuild.sub;
+
+public class OtherClass {  
+    public String msg() {
+        return "Hello world.";
+    }
+}
