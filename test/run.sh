0302999ef7f963c8fe479058718d8451afe0ede8
Ric Allinson <allinson@yahoo-inc.com>
2016-01-23 15:56:34 -0800

    Cleanup of test code

--- test/run.sh
+++ test/run.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-
 cd "$(dirname "$0")"
 export JMMPATH=$(dirname $(pwd))/examples
+echo
+echo "Running Tests"
+echo
 echo "JMMPATH is set to $JMMPATH"
 source ../jmm.sh
-
+echo
 for test in ./functional/*.sh; do
 	jmm clean
 	$test
 	if [ $? -eq 0 ]; then
-	  echo "Pass $test"
+	  echo "    Pass $test"
 	else
-	  echo "Fail $test"
+	  echo "    Fail $test"
 	fi
 	jmm clean
 done
+echo
