3df29b7bed8f71966fd55bfb5ced406fa239b476
Ric Allinson <allinson@yahoo-inc.com>
2016-01-22 23:02:37 -0800

    Looking for main method in build command

--- jmm.sh
+++ jmm.sh
@@ -68,11 +68,16 @@ jmm_helper_build_jar() {
 jmm_build() {
     # need to find all dependancies and add those files too
     # need to find the file that has the main method and make that first in the list
+    main=""
     files=""
     for file in $(find $1 -name '*.java' ); do
-        files="$files $file"
+        if [ -z $main ] && grep -q main "$file"; then
+            main="$file"
+        else
+            files="$files $file"
+        fi
     done
-    jmm_helper_build_jar $files
+    jmm_helper_build_jar $main $files
 }
 
 jmm_clean() {
