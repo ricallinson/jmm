46614c71b28ffd77fc8eb34be834cd0018063f55
Ric Allinson <allinson@yahoo-inc.com>
2016-01-23 16:08:33 -0800

    Making travis work

--- test/run.sh
+++ test/run.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 export JMMPATH=$(dirname $(pwd))/examples
+
 echo
 echo "Running Tests"
 echo
 echo "JMMPATH is set to $JMMPATH"
 source ../jmm.sh
 echo
+
+failures=0
+successes=0
+
 for test in ./functional/*.sh; do
 	jmm clean
 	$test
 	if [ $? -eq 0 ]; then
-	  echo "    Pass $test"
+		successes=successes+1
+	    echo "    Pass $test"
 	else
-	  echo "    Fail $test"
+		failures=failures+1
+	    echo "    Fail $test"
 	fi
 	jmm clean
 done
+
 echo
+
+if [[ failures != 0 ]]; then
+	exit 1
+fi
+exit 0
