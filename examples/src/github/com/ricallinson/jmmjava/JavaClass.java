a06a2347f7712a24eb59485c1c65df720a1eba31
Ric Allinson <allinson@yahoo-inc.com>
2016-01-24 23:25:19 -0800

    Fixing build

--- examples/src/github/com/ricallinson/jmmjava/JavaClass.java
+++ examples/src/github/com/ricallinson/jmmjava/JavaClass.java
@@ -1,12 +1,12 @@
 package github.com.ricallinson.jmmjava;
 
-import java.time.*;
+import java.time.Year;
 import github.com.ricallinson.jmmbuild.sub.*;
 
 public class JavaClass {  
     public static void main(String args[]) {
         OtherClass c = new OtherClass();
-        String time = java.time.format.DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now(ZoneId.of("GMT")));
+        int time = Year.MAX_VALUE;
         System.out.println(c.msg());
         System.out.println("Here's one random number: " + Math.random());
     }
