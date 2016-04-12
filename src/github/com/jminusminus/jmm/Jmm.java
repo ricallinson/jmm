d1f609947202b7602e6a02e24f363ff18d5a9f89
Ric Allinson <allinson@yahoo-inc.com>
2016-04-12 00:00:50 -0700

    Started on Jmm version of Jmm

---
+++ src/github/com/jminusminus/jmm/Jmm.java
@@ -0,0 +1,17 @@
+//
+// Copyright 2016, Yahoo Inc.
+// Copyrights licensed under the New BSD License.
+// See the accompanying LICENSE file for terms.
+//
+
+// [![Build Status](https://travis-ci.org/jminusminus/jmm.svg?branch=master)](https://travis-ci.org/jminusminus/jmm)
+// ## Stability: 0 - Unstable
+// This package contains the CLI for the Jmm tool.
+package github.com.jminusminus.jmm;
+
+public class Jmm {
+
+	public static void main(String[] args) {
+        
+    }
+}
